6903a36f88f924424c166764967ad573ae2fcfd6
I Gusti Agung Ngurah Panji Palguna <dev4@dimata.com>
2025-10-02 14:38:41 +0800

    update

---
+++ src/main/java/com/example/crud/AppShell.java
@@ -0,0 +1,21 @@
+package com.example.crud;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.AppShellSettings;
+import com.vaadin.flow.server.PWA;
+
+@PWA(
+    name = "CRUD Vaadin",
+    shortName = "CRUD",
+    iconPath = "images/logo.jpeg" // ini untuk PWA installable app
+)
+public class AppShell implements AppShellConfigurator {
+
+    @Override
+    public void configurePage(AppShellSettings settings) {
+        // favicon utama
+        settings.addFavIcon("icon", "images/logo.jpeg", "32x32");
+
+        // bisa juga tambahan ukuran lain (opsional)
+    }
+}
